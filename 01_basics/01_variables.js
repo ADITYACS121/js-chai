9b0aee622ce60f9b7f4fc8ddc37a816f7601e649
ADITYACS121 <119475155+ADITYACS121@users.noreply.github.com>
2024-05-14 08:56:38 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553 //isko change nhi kar sakte const
+let accountEmail = "adityabharti0101@gmail.com"
+var accpass = "9505"
+accountCity ="Una" 
+
+
+accountEmail = "pec.edu.in";
+accpass = "9705"
+accountCity = "Nangal"
+
+/*prefered not use var because of use in the block and functional scope*/
+
+
+
+console.table([accountId , accountEmail , accpass , accountCity ]);
+
