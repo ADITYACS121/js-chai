426c4425a65dfcf6d326c93552da11ff7ca9f9ee
ADITYACS121 <119475155+ADITYACS121@users.noreply.github.com>
2024-05-14 08:57:14 +0000

    conversion

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,34 @@
+
+let score = "33abc"
+
+console.log(typeof(score))
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber))
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => true
+
+let loggedIn = 1
+
+console.log(typeof loggedIn)
+
+let booleanLoggedIn = Boolean(loggedIn);
+
+console.log(typeof booleanLoggedIn)
+
+let someNumber = 33
+
+console.log(someNumber)
+console.log(typeof someNumber)
+
+let toString = String(someNumber);
+
+console.log(typeof toString)
