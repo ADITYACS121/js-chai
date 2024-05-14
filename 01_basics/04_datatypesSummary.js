5bc079273cc58fc62dc87fff7e62a4c462c5ede0
ADITYACS121 <119475155+ADITYACS121@users.noreply.github.com>
2024-05-14 10:42:31 +0000

    datatypes

---
+++ 01_basics/04_datatypesSummary.js
@@ -0,0 +1,20 @@
+//Primitive data types 
+
+//7 categories  :: call by value
+//String 
+//Number 
+//Boolean
+//null
+//undefined
+//Symbol
+//BigInt
+
+
+//Reference types 
+//Array 
+//Objects 
+//Functions
+
+
+//js dynamically typed or statically typed
+
